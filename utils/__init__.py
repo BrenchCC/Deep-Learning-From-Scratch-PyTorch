6bbac65951b4181d9a5d5d385f44e51c0b455e9e
BrenchCC <brenchchen.77@gmail.com>
2025-12-30 15:07:55 +0800

    fix:fix a file path

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,4 +3,4 @@ from .device import get_device
 from .file_io_util import save_json, load_json, save_pickle
 from .model_summary import count_parameters, log_model_info, log_model_info_from_path
 from .timer import Timer
-from .graph_visualization import build_dot
+from ..chapter_01_tensor_autograd.graph_visualization import build_dot
